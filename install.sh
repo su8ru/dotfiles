1e30ac88d7b28baf6e00c08d5ff0d0fe90bfd2cf
subaru <main@su8ru.dev>
2025-04-17 11:41:37 +0900

    chore: fix install script

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ find . -type f | cut -b 3- | xargs -I '{}' ln -snfv "$HOME/dotfiles/dotfiles/{}"
 find . -type d | cut -b 3- | xargs -I '{}' chmod 755 "$HOME/{}"
 find . -type f | cut -b 3- | xargs -I '{}' chmod 755 "$HOME/{}"
 
-chmod 700 -R "$HOME/dotfiles"
+chmod -R 700 "$HOME/dotfiles"
 chmod 600 "$HOME/dotfiles/dotfiles/.ssh/authorized_keys"
 
-echo "Dotfiles install success"
+echo "done"
+
